5f54a3266185b08193b9505f001eaca5e2ff3db5
Tmcclellan12 <86691088+Tmcclellan12@users.noreply.github.com>
2022-03-19 18:32:05 -0500

    Add files via upload

---
+++ aoc.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://www.ocasiocortez.com/issues#housing-as-a-human-right")
+soup = BeautifulSoup(page.content,'html.parser')
+mylist = soup.find_all(class_="issue-footnotes")
+
+lis = []
+for ul in mylist:
+    for li in ul.findAll('li'):
+        if li.find('ul'):
+            break
+        lis.append(li)
+
+for li in lis: 
+    print (li.text.encode("utf-8"))
